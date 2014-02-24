5ce3f5fcaddda1c9c56ffd31e135867f1979b173
gsick <gsick@emailvision.com>
2014-02-24 09:34:55 +0100

    remove directory create by installer

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,17 +13,10 @@
 
 class openam::config {
   $server_url = "${openam::server_protocol}://${openam::host}:${openam::server_port}"
- 
+
   package { "perl-Crypt-SSLeay": ensure => installed }
   package { "perl-libwww-perl": ensure => installed }
-  
-  file { "${openam::deploy_container_home}/.openamcfg":
-    ensure => directory,
-    owner  => "${openam::deploy_container_user}",
-    group  => "${openam::deploy_container_group}",
-    mode   => 755,
-  }
- 
+
   # Contains passwords, thus (temporarily) stored in ${openam::tmp}
   file { "${openam::tmp}/configurator.properties":
     owner   => root,
@@ -40,19 +33,11 @@ class openam::config {
     source  => "puppet:///modules/${module_name}/configurator.pl",
   }
 
-  file { "${openam::config_dir}":
-    ensure => directory,
-    owner  => "${openam::deploy_container_user}",
-    group  => "${openam::deploy_container_group}",
-  }
-
   exec { "configure openam":
     command => "${openam::tmp}/configurator.pl -f ${openam::tmp}/configurator.properties",
     require => [
       File["${openam::tmp}/configurator.pl"],
-      File["${openam::config_dir}"],
-      Package["perl-Crypt-SSLeay"],
-      Package["perl-libwww-perl"]
+      Package["perl-Crypt-SSLeay", "perl-libwww-perl"],
     ],
     creates => "${openam::config_dir}/bootstrap",
     notify => Service["tomcat-openam"],
